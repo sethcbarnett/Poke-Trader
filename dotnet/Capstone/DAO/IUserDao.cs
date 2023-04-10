b272e17347c3a6893dbd74b92d006b00eae6e239
John Lindsey <johnlindseyjl14@gmail.com>
2023-04-10 16:18:05 -0400

    Added additional properties for user information on registration in back-end

--- dotnet/Capstone/DAO/IUserDao.cs
+++ dotnet/Capstone/DAO/IUserDao.cs
@@ -5,6 +5,6 @@ namespace Capstone.DAO
     public interface IUserDao
     {
         User GetUser(string username);
-        User AddUser(string username, string password, string role);
+        User AddUser(string username, string password, string role, string email, string streetAddress, string city, string stateAbbreviation, int zipCode);
     }
 }
