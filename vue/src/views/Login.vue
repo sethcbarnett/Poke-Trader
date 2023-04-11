838e6743e1e16a28e8f0bb21468cead8dcb36474
John Lindsey <johnlindseyjl14@gmail.com>
2023-04-11 13:46:03 -0400

    More styling of nav bar, and authentication

--- vue/src/views/Login.vue
+++ vue/src/views/Login.vue
@@ -46,7 +46,7 @@ export default {
           if (response.status == 200) {
             this.$store.commit("SET_AUTH_TOKEN", response.data.token);
             this.$store.commit("SET_USER", response.data.user);
-            this.$router.push("/");
+            this.$router.push("/collection");
           }
         })
         .catch(error => {
