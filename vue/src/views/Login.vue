c4d65350bf0b8779bac95b54fe7fc38c5d246942
John Lindsey <johnlindseyjl14@gmail.com>
2023-04-13 10:07:19 -0400

    Login takes user to homepage. New user successfully creates new collection in database

--- vue/src/views/Login.vue
+++ vue/src/views/Login.vue
@@ -47,7 +47,7 @@ export default {
             this.$store.commit("SET_AUTH_TOKEN", response.data.token);
             this.$store.commit("SET_USER", response.data.user);
             this.$store.commit("SET_PREMIUM", response.data.user);
-            this.$router.push("/collection");
+            this.$router.push("/");
           }
         })
         .catch(error => {
