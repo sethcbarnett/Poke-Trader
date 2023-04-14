80fdf6fb6c67d5ebbf43402810ac3c04c2dbb771
Patrick Kilian <pjkkilian@gmail.com>
2023-04-14 11:02:14 -0400

    Rarity & Full Price Info through Back End

--- dotnet/Capstone/Models/Card.cs
+++ dotnet/Capstone/Models/Card.cs
@@ -6,14 +6,20 @@
         public string Name { get; set; }
         public string Img { get; set; }
         public string Price { get; set; }
+        public string LowPrice { get; set; }
+        public string HighPrice { get; set; }
+        public string Rarity { get; set; }
         public string TcgUrl { get; set; }
 
-        public Card(string id, string name, string img, string price, string tcgUrl) 
+        public Card(string id, string name, string img, string price, string lowPrice, string highPrice, string rarity, string tcgUrl) 
         {
             this.Id = id;
             this.Name = name;
             this.Img = img;
             this.Price = price;
+            this.LowPrice = lowPrice;
+            this.HighPrice = highPrice;
+            this.Rarity = rarity;
             this.TcgUrl= tcgUrl;
         }
 
