377d443599ce0efea2097bb1155f9dbc0e349251
John Lindsey <johnlindseyjl14@gmail.com>
2023-04-17 16:35:48 -0400

    Adjusted some containers for trade page

--- vue/src/components/UserTradeWindow.vue
+++ vue/src/components/UserTradeWindow.vue
@@ -1,6 +1,14 @@
 <template>
   <div id="user-window">
-      <h3>{{ this.username }}'s Cards here</h3>
+      <h3>{{ this.username }} will give:</h3>
+      <h4>Est. Value: </h4>
+      <div id="cards-for-trade">
+          <h5>Charizard</h5>
+      </div>
+      
+      <div id="collection">
+          <h5>Alakazam</h5>
+      </div>
   </div>
 </template>
 
@@ -16,9 +24,30 @@ export default {
     border-style: solid;
     margin: 5px;
     flex-basis: 50%;
-    height: 70vh;
+    height: 95%;
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+    justify-content: space-around;
+}
+#cards-for-trade {
+    flex-basis: 50%;
+    border-style: solid;
+    width: 90%;
+}
+#collection {
+    flex-basis: 30%;
+    border-style: solid;
+    width: 90%;
+}
+h4 {
+    margin: 0;
+    align-self: flex-start;
+    margin-left: 5px;
+    flex-basis: 2%;
 }
 h3 {
-    text-align: center;
+    margin: 0;
+    flex-basis: 5%;
 }
 </style>
