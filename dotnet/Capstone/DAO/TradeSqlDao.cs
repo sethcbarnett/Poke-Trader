5e33eb61383cb433dc067dc30946aef448c52271
Eli Weiskirch <weiskirche@gmail.com>
2023-04-18 14:51:16 -0400

    Added add new trade at /trade/{u1}/{u2} but not tested until we do GET at same address

--- dotnet/Capstone/DAO/TradeSqlDao.cs
+++ dotnet/Capstone/DAO/TradeSqlDao.cs
@@ -19,6 +19,67 @@ namespace Capstone.DAO
             connectionString = dbConnectionString;
         }
 
+        public string AddTrade(Trade trade)
+        {
+            using (SqlConnection conn = new SqlConnection(connectionString))
+            {
+                conn.Open();
+                //See if there is already a trade between the active users
+
+                SqlCommand isTradeAlreadyActive = new SqlCommand("SELECT * FROM trade WHERE (user_id_from = (SELECT user_id FROM users WHERE username = @userOne) " +
+                                                                 "AND user_id_to = (SELECT user_id FROM users WHERE username = @userTwo)) OR " +
+                                                                 "(user_id_from = (SELECT user_id FROM users WHERE username = @userTwo) " +
+                                                                 "AND user_id_to = (SELECT user_id FROM users WHERE username = @userOne)) ", conn);
+                isTradeAlreadyActive.Parameters.AddWithValue("@userOne", trade.UsernameFrom);
+                isTradeAlreadyActive.Parameters.AddWithValue("@userTwo", trade.UsernameTo);
+                SqlDataReader hasOpenTrade = isTradeAlreadyActive.ExecuteReader();
+                if (hasOpenTrade.Read())
+                {
+                    return "These users already have an open trade.";
+                }
+
+                //Add trade to trade table
+                SqlCommand addToTradeTable = new SqlCommand("INSERT INTO trade (user_id_from, user_id_to) " +
+                                                            "OUTPUT INSERTED.trade_id " +
+                                                            "VALUES " +
+                                                            "((SELECT user_id FROM users WHERE username = @userOne), " +
+                                                            "(SELECT user_id FROM users WHERE username = @userTwo));", conn);
+                addToTradeTable.Parameters.AddWithValue("@userOne", trade.UsernameFrom);
+                addToTradeTable.Parameters.AddWithValue("@userTwo", trade.UsernameTo);
+
+                int tradeId = Convert.ToInt32(addToTradeTable.ExecuteNonQuery());
+
+                //Add collection Items From to the trade_card_collection join table
+                foreach (CollectionItem item in trade.CollectionItemsFrom)
+                {
+                    SqlCommand addCollectionItemsFrom = new SqlCommand("INSERT INTO trade_card_collection (trade_id, id, collection_id) VALUES (@tradeId," +
+                                                                       "(SELECT id FROM card where name = @cardName)," +
+                                                                       "(SELECT collection_id FROM collection WHERE user_id = " +
+                                                                       "(SELECT user_id from users WHERE username = @username)));", conn);
+                    addCollectionItemsFrom.Parameters.AddWithValue("@tradeId", tradeId);
+                    addCollectionItemsFrom.Parameters.AddWithValue("@cardName", item.Card.Name);
+                    addCollectionItemsFrom.Parameters.AddWithValue("@username", trade.UsernameFrom);
+
+                    addCollectionItemsFrom.ExecuteNonQuery();
+                }
+
+                //Add collection Items To to the trade_card_collection join table
+                foreach (CollectionItem item in trade.CollectionItemsTo)
+                {
+                    SqlCommand addCollectionItemsFrom = new SqlCommand("INSERT INTO trade_card_collection (trade_id, id, collection_id) VALUES (@tradeId," +
+                                                                       "(SELECT id FROM card where name = @cardName)," +
+                                                                       "(SELECT collection_id FROM collection WHERE user_id = " +
+                                                                       "(SELECT user_id from users WHERE username = @username)));", conn);
+                    addCollectionItemsFrom.Parameters.AddWithValue("@tradeId", tradeId);
+                    addCollectionItemsFrom.Parameters.AddWithValue("@cardName", item.Card.Name);
+                    addCollectionItemsFrom.Parameters.AddWithValue("@username", trade.UsernameTo);
+
+                    addCollectionItemsFrom.ExecuteNonQuery();
+                }
+            }
+            return "Hooray";
+        }
+
         public Trade GetTrade(int tradeId)
         {
             return null;
@@ -26,7 +87,6 @@ namespace Capstone.DAO
 
         public Trade AttemptTrade(Trade trade)
         {
-
             return trade;
         }
         public void UpdateTrade(Trade trade)
