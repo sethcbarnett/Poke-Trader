69bc35922424bfee8edd8eed83b0ecaf9bb49b69
Patrick Kilian <pjkkilian@gmail.com>
2023-04-19 16:27:31 -0400

    Fixed More Merge Conflicts

--- vue/src/components/UserTradeWindow.vue
+++ vue/src/components/UserTradeWindow.vue
@@ -3,12 +3,11 @@
     <h3>{{ this.username }} will give:</h3>
     <h4>Est. Value: ${{ AddAllCardValues }}</h4>
 
-      
-      <div id="cards-for-trade">
-          <div id="trades-scroll">
-          <simplified-card-display
-          v-bind:availableCard="availableCard" 
-          v-bind:username="username" 
+    <div id="cards-for-trade">
+      <div id="trades-scroll">
+        <simplified-card-display
+          v-bind:availableCard="availableCard"
+          v-bind:username="username"
           :clickAction="MakeCardUnproposed"
           v-for="availableCard in CheckProposedCards"
           v-bind:key="availableCard.card.id"
@@ -18,9 +17,9 @@
 
     <div id="collection">
       <div id="collection-scroll">
-          <simplified-card-display 
-          v-bind:availableCard="availableCard" 
-          v-bind:username="username" 
+        <simplified-card-display
+          v-bind:availableCard="availableCard"
+          v-bind:username="username"
           :clickAction="MakeCardProposed"
           v-for="availableCard in CheckAvailableCards"
           v-bind:key="availableCard.card.id"
@@ -34,18 +33,19 @@
 import SimplifiedCardDisplay from "./SimplifiedCardDisplay.vue";
 
 export default {
-    name: 'user-trade-window',
-    props: ['username'],
-    components: { SimplifiedCardDisplay },
-    data() {
-        return {
-            availableCard: {},
-            newUsername: '',
-            MakeCardProposed: "MakeCardProposed",
-            MakeCardUnproposed: "MakeCardUnproposed",
-            DoNothing: "DoNothing"
-        }
-    },
+  name: "user-trade-window",
+  props: ["username"],
+  components: { SimplifiedCardDisplay },
+  data() {
+    return {
+      availableCard: {},
+      newUsername: "",
+      MakeCardProposed: "MakeCardProposed",
+      MakeCardUnproposed: "MakeCardUnproposed",
+      DoNothing: "DoNothing",
+    };
+  },
+  methods: {
     SetNewUsername() {
       this.newUsername = this.username;
     },
@@ -116,7 +116,7 @@ export default {
   color: black;
   border-width: 2px;
   border-radius: 10px;
-  min-height:227.5px;
+  min-height: 227.5px;
   gap: 2px;
   display: flex;
   flex-direction: row;
@@ -132,8 +132,6 @@ h4 {
 h3 {
   margin: 0;
   flex-basis: 5%;
-  
-
 }
 
 #collection-scroll::-webkit-scrollbar {
@@ -153,15 +151,15 @@ h3 {
 #collection-scroll {
   display: flex;
   justify-content: flex-start;
-  align-self:flex-start;
+  align-self: flex-start;
   flex-direction: row;
   overflow-y: hidden;
   overflow-x: auto;
-  
+
   max-width: 36vw;
   flex-wrap: nowrap;
-  
-  padding-top:2.5px;
+
+  padding-top: 2.5px;
 }
 #trades-scroll::-webkit-scrollbar {
   width: 20px;
